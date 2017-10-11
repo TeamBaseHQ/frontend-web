b0cb94ba95cdf928a200752a9bcee56f9f08e99f
Kunal Varma <kunalvarma05@gmail.com>
2017-10-11 18:05:55 +0530

    Sample Dashboard Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '../pages/Home';
+import Dashboard from '../pages/Dashboard';
 import AppBase from '../pages/app/AppBase';
 import ChannelThreads from '../pages/app/ChannelThreads';
 
@@ -13,6 +14,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
     },
     {
       path: '/app',
