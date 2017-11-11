d6833755d5a705b730787494b7cdaec53a61ff7c
Kunal Varma <kunalvarma05@gmail.com>
2017-11-11 20:13:56 +0530

    Navbar Menu Items

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -9,6 +9,29 @@
   >
     <team-menu></team-menu>
     <v-spacer></v-spacer>
+    <div>
+      <v-btn flat small :to="{ name: 'app' }" exact>
+        <v-icon left>list</v-icon>
+        <span>Channels</span>
+      </v-btn>
+      <v-btn flat small>
+        <v-icon left>comment</v-icon>
+        <span>Conversations</span>
+      </v-btn>
+      <v-btn flat small>
+        <v-icon left>people</v-icon>
+        <span>Team</span>
+      </v-btn>
+      <v-btn flat small>
+        <v-icon left>folder</v-icon>
+        <span>Files</span>
+      </v-btn>
+      <v-btn flat small>
+        <v-icon left>search</v-icon>
+        <span>Search</span>
+      </v-btn>
+    </div>
+    <v-spacer></v-spacer>
     <v-btn icon>
       <v-icon>apps</v-icon>
     </v-btn>
