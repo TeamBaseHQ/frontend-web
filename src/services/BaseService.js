320cb518e9f271071f2f1985b4623179e13506b0
Kunal Varma <kunalvarma05@gmail.com>
2017-11-13 05:30:27 +0530

    BaseService class

---
+++ src/services/BaseService.js
@@ -0,0 +1,13 @@
+import {Base, BaseApp} from 'base-javascript-sdk';
+import ENV from '../env';
+
+export default class BaseService {
+  static base() {
+    if (BaseService.baseInstance instanceof Base) {
+      return BaseService.baseInstance;
+    }
+
+    BaseService.baseInstance = new Base(new BaseApp(ENV.client_id, ENV.client_secret, ENV.api_url));
+    return BaseService.baseInstance;
+  }
+}
