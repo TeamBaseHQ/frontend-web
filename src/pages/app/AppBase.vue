bb364ee11f897bd32b1fcd0de574a2e80fa07c85
Ronakdelhiwala <Ronakdelhiwala@gmail.com>
2017-11-16 02:30:10 +0530

    finally adding app category component to app vue

--- src/pages/app/AppBase.vue
+++ src/pages/app/AppBase.vue
@@ -2,7 +2,7 @@
   <v-app id="app-base" class="app-page app-base" :dark="isDarkMode">
     <SideBar></SideBar>
     <NavBar></NavBar>
-
+    <category-list></category-list>
     <main class="app-main">
       <v-content>
         <router-view></router-view>
@@ -14,12 +14,14 @@
 <script>
   import SideBar from '../../components/SideBar';
   import NavBar from '../../components/NavBar/NavBar';
+  import CategoryList from '../../components/CategoryList';
 
   export default {
     name: 'AppBase',
     components: {
       NavBar,
       SideBar,
+      CategoryList,
     },
     created() {
       //
