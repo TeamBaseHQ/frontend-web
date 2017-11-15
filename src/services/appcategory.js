973e7a8e9ea2b6cd2475e4147afd924499c69361
Ronakdelhiwala <Ronakdelhiwala@gmail.com>
2017-11-16 02:29:06 +0530

    dubmy stubs for app category

---
+++ src/services/appcategory.js
@@ -0,0 +1,18 @@
+export default {
+  all() {
+    return {
+      productivity: {
+        name: 'Productivity Tools',
+        icon: 'work',
+      },
+      developer_tools: {
+        name: 'Developer Tools',
+        icon: 'computer',
+      },
+      fun: {
+        name: 'Fun',
+        icon: 'insert_emoticon',
+      },
+    };
+  },
+};
