9164394aecb358605b9b94e72b359e948a731e8c
Kunal Varma <kunalvarma05@gmail.com>
2017-11-21 02:32:38 +0530

    Fix Auth lib errors

--- src/lib/Auth.js
+++ src/lib/Auth.js
@@ -1,15 +1,15 @@
 export default {
   setAccessToken(accessToken) {
-    window.localStorage.set('_token', JSON.stringify(accessToken));
+    window.localStorage.setItem('_token', JSON.stringify(accessToken));
   },
   getAccessToken() {
-    return JSON.parse(window.localStorage.get('_token'));
+    return JSON.parse(window.localStorage.getItem('_token'));
   },
   setCurrentUser(user) {
-    window.localStorage.set('_currentUser', JSON.stringify(user));
+    window.localStorage.setItem('_currentUser', JSON.stringify(user));
   },
   getCurrentUser() {
-    return JSON.parse(window.localStorage.get('_currentUser'));
+    return JSON.parse(window.localStorage.getItem('_currentUser'));
   },
   isAuthenticated() {
     return this.getAccessToken() && this.getCurrentUser();
