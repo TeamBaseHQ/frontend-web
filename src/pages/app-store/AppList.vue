1fcc84287394de1a3955ac113c0c5b998f105b45
Kunal Varma <kunalvarma05@gmail.com>
2017-11-21 01:01:18 +0530

    Fix variable name in loop

--- src/pages/app-store/AppList.vue
+++ src/pages/app-store/AppList.vue
@@ -2,36 +2,37 @@
   <div>
     <br>
     <v-layout row>
-    <v-flex xs12 sm10 offset-sm1>
-    <v-card>
-      <v-list two-lines >
-        <!-- <br> -->
-        <!-- <v-divider v-bind:inset="app.inset"></v-divider> -->
-        <v-list-tile :to="{ name: 'app-store-details', params: { app: appSlug} }" v-for="(app, appSlug) in allAppsByCategory" :key="app.name">
-          <v-list-tile-action>
-            <v-list-tile-avatar>
-                <img :src="app.image" alt="logo">
-            </v-list-tile-avatar>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{app.name}}</v-list-tile-title>
-            <br>
-            <v-divider></v-divider>
-          </v-list-tile-content>
-          <!-- <v-divider></v-divider> -->
-          <v-icon>keyboard_arrow_right</v-icon>
+      <v-flex xs12 sm10 offset-sm1>
+        <v-card>
+          <v-list two-lines>
+            <!-- <br> -->
+            <!-- <v-divider v-bind:inset="app.inset"></v-divider> -->
+            <v-list-tile :to="{ name: 'app-store-details', params: { appSlug: slug } }"
+                         v-for="(app, slug) in allAppsByCategory" :key="app.name">
+              <v-list-tile-action>
+                <v-list-tile-avatar>
+                  <img :src="app.image" alt="logo">
+                </v-list-tile-avatar>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>{{app.name}}</v-list-tile-title>
+                <br>
+                <v-divider></v-divider>
+              </v-list-tile-content>
+              <!-- <v-divider></v-divider> -->
+              <v-icon>keyboard_arrow_right</v-icon>
 
-        </v-list-tile>
-      <!-- <v-divider></v-divider> -->
-      </v-list>
-    </v-card>
-    </v-flex>
+            </v-list-tile>
+            <!-- <v-divider></v-divider> -->
+          </v-list>
+        </v-card>
+      </v-flex>
     </v-layout>
   </div>
 </template>
 
 <script>
- // import apps from '../../services/app-store/apps';
+  // import apps from '../../services/app-store/apps';
   import {mapGetters, mapActions} from 'vuex';
 
   export default {
