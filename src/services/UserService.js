44b3b8257ccf28bda5ca56ec2634154770df64ea
Kunal Varma <kunalvarma05@gmail.com>
2017-11-21 03:35:19 +0530

    Delete obsolete UserService class

--- src/services/UserService.js
+++
@@ -1,7 +0,0 @@
-import BaseService from './BaseService';
-
-export default class UserService extends BaseService {
-  static find(id) {
-    return UserService.base().get(`/users/${id}`).then(response => UserService.getData(response));
-  }
-}
