1cf72dd18e6a27e0424da42115c116c75d97a349
Kunal Varma <kunalvarma05@gmail.com>
2017-11-21 04:34:43 +0530

    Auth Service: Make and return class objects

--- src/lib/Auth.js
+++ src/lib/Auth.js
@@ -1,15 +1,30 @@
+import {Auth as Authentication, Models} from 'base-javascript-sdk';
+
 export default {
   setAccessToken(accessToken) {
     window.localStorage.setItem('_token', JSON.stringify(accessToken));
   },
   getAccessToken() {
-    return JSON.parse(window.localStorage.getItem('_token'));
+    const token = JSON.parse(window.localStorage.getItem('_token'));
+
+    if (!token) {
+      return false;
+    }
+    return new Authentication.AccessToken(
+      token.access_token, token.expires_in, token.refresh_token,
+    );
   },
   setCurrentUser(user) {
     window.localStorage.setItem('_currentUser', JSON.stringify(user));
   },
   getCurrentUser() {
-    return JSON.parse(window.localStorage.getItem('_currentUser'));
+    const data = JSON.parse(window.localStorage.getItem('_currentUser'));
+
+    if (!data) {
+      return false;
+    }
+
+    return new Models.User(data);
   },
   isAuthenticated() {
     return this.getAccessToken();
