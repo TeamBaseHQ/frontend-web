098b7318e4608a549f020cd30aee659f9faed0fd
Kunal Varma <kunalvarma05@gmail.com>
2017-11-21 03:37:30 +0530

    AuthService: store AccessToken using Auth lib after fetching

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,7 +1,11 @@
 import BaseService from './BaseService';
+import Auth from '../lib/Auth';
 
 export default class AuthService extends BaseService {
   static getUserAccessToken(email, password) {
-    return super.base().getUserAccessToken(email, password);
+    return super.base().getUserAccessToken(email, password).then((accessToken) => {
+      Auth.setAccessToken(accessToken);
+      return accessToken;
+    });
   }
 }
