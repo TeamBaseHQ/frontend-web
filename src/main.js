7d7b71cdf0c281a253b3abe3aad916a4f327d80c
Kunal Varma <kunalvarma05@gmail.com>
2017-11-21 02:16:58 +0530

    Uninstall vue localstorage

--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import VeeValidate from 'vee-validate';
-import VueLocalStorage from 'vue-localstorage';
 import App from './App';
 import store from './store/index';
 import router from './router';
 import Auth from './mixins/Auth';
 import DarkMode from './mixins/DarkMode';
-import LocalStorage from './mixins/LocalStorage';
 
 Vue.config.productionTip = false;
-
-Vue.use(VueLocalStorage);
 Vue.use(Vuetify);
 
 const validatorConfig = {
@@ -19,8 +15,6 @@ const validatorConfig = {
 };
 
 Vue.use(VeeValidate, validatorConfig);
-
-Vue.use(LocalStorage);
 Vue.mixin(Auth);
 Vue.mixin(DarkMode);
 
