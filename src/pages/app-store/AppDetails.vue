2e863e4916bc686b82486a1118bec2884580ecac
dimple238 <30922169+dimple238@users.noreply.github.com>
2017-11-20 19:15:34 +0530

    added a component to render app specific details

---
+++ src/pages/app-store/AppDetails.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+     <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-card-media :src="appDetails.image" height="200px">
+        </v-card-media>
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">{{appDetails.name}}</h3>
+            <div>{{appDetails.description}}</div>
+          </div>
+        </v-card-title>
+      </v-card>
+    </v-flex>
+  </v-layout>
+
+  </div>
+
+
+</template>
+
+<script>
+  import {mapGetters, mapActions} from 'vuex';
+
+  export default {
+    name: 'app-store-details',
+    created() {
+      this.fetchAppDetails(this.appSlug);
+    },
+    computed: {
+      ...mapGetters({
+        appDetails: 'currentApp',
+      }),
+      appSlug() {
+        return this.$route.params.appSlug;
+      },
+    },
+    watch: {
+      $route() {
+        this.fetchAppDetails(this.appSlug);
+      },
+    },
+    methods: {
+      ...mapActions([
+        'fetchAppDetails',
+      ]),
+    },
+  };
+</script>
